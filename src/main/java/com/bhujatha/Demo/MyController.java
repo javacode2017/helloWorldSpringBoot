2f20c9867adb52bd41f0c9c02084b92a4edad3c5
sujith <sujithreddy440@gmail.com>
2020-02-12 21:04:21 +0530

    initial commit

---
+++ src/main/java/com/bhujatha/Demo/MyController.java
@@ -0,0 +1,14 @@
+package com.bhujatha.Demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping("/")
+	public String hello() {
+		
+		return "hellllllllloooooooo";
+	}
+}
